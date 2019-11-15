aaca3de9c699a95c8ff628ba2de5706bf50803e9
Anastasiia Gorbenko <nstgorbenko@yandex.ru>
2019-11-16 00:07:11 +0300

    Добавлена маска полям ввода номера телефона

--- source/js/main.js
+++ source/js/main.js
@@ -1,32 +1,45 @@
 'use strict';
 
-var link = document.querySelector('.main-header__button');
+var PHONE_MASK = {
+  mask: '+{7}(000)000-00-00'
+};
+
+var phoneCall = document.querySelector('.main-header__button');
 var overlay = document.querySelector('.overlay');
+
 var popup = document.querySelector('.call');
-var close = popup.querySelector('.call__close');
-var form = popup.querySelector('.call__letter');
-var fio = popup.querySelector('[name=name]');
-var tel = popup.querySelector('[name=tel]');
-var message = popup.querySelector('[name=message]');
+var popupCloser = popup.querySelector('.call__close');
+var popupForm = popup.querySelector('.call__letter');
+var popupName = popup.querySelector('[name=popup-name]');
+var popupPhone = popup.querySelector('[name=popup-phone]');
+var popupMessage = popup.querySelector('[name=popup-message]');
+
+var phone = document.querySelector('[name=phone]');
+
 var navList = document.querySelector('.dropdown__checkbox-nav');
 var contactsList = document.querySelector('.dropdown__checkbox-contacts');
+
 var isStorageSupport = true;
-var nameStorage = '';
-var telStorage = '';
-var messageStorage = '';
+var popupNameStorage = '';
+var popupPhoneStorage = '';
+var popupMessageStorage = '';
+
+var setMask = function (phoneInput) {
+  IMask(phoneInput, PHONE_MASK);
+};
+
+var openPopup = function () {
+  popup.classList.add('call__show');
+  overlay.classList.add('overlay__show');
+  popupName.focus();
+  document.body.style.overflow = 'hidden';
+};
 
 var closePopup = function () {
   popup.classList.remove('call__show');
   popup.classList.remove('call__error');
   overlay.classList.remove('overlay__show');
   document.body.style.overflow = 'visible';
-};
-
-var openPopup = function () {
-  popup.classList.add('call__show');
-  overlay.classList.add('overlay__show');
-  fio.focus();
-  document.body.style.overflow = 'hidden';
 };
 
 var onDropdownToggleClick = function (evt) {
@@ -40,26 +53,29 @@ var onDropdownToggleClick = function (evt) {
 navList.addEventListener('change', onDropdownToggleClick);
 contactsList.addEventListener('change', onDropdownToggleClick);
 
+setMask(phone, PHONE_MASK);
+setMask(popupPhone, PHONE_MASK);
+
 try {
-  nameStorage = localStorage.getItem('fio');
-  telStorage = localStorage.getItem('tel');
-  messageStorage = localStorage.getItem('message');
+  popupNameStorage = localStorage.getItem('popupName');
+  popupPhoneStorage = localStorage.getItem('popupPhone');
+  popupMessageStorage = localStorage.getItem('popupMessage');
 } catch (err) {
   isStorageSupport = false;
 }
 
-link.addEventListener('click', function (evt) {
+phoneCall.addEventListener('click', function (evt) {
   evt.preventDefault();
   openPopup();
 
-  if (nameStorage) {
-    fio.value = nameStorage;
-    tel.value = telStorage;
-    message.value = messageStorage;
+  if (popupNameStorage) {
+    popupName.value = popupNameStorage;
+    popupPhone.value = popupPhoneStorage;
+    popupMessage.value = popupMessageStorage;
   }
 });
 
-close.addEventListener('click', function (evt) {
+popupCloser.addEventListener('click', function (evt) {
   evt.preventDefault();
   closePopup();
 });
@@ -78,17 +94,19 @@ window.addEventListener('keydown', function (evt) {
   }
 });
 
-form.addEventListener('submit', function (evt) {
-  if (!fio.value || !tel.value || !message.value) {
+popupForm.addEventListener('submit', function (evt) {
+  if (!popupName.value || !popupPhone.value || !popupMessage.value) {
     evt.preventDefault();
     popup.classList.remove('call__error');
     popup.width = popup.offsetWidth;
     popup.classList.add('call__error');
   } else {
     if (isStorageSupport) {
-      localStorage.setItem('name', fio.value);
-      localStorage.setItem('tel', tel.value);
-      localStorage.setItem('message', message.value);
+      localStorage.setItem('popupName', popupName.value);
+      localStorage.setItem('popupPhone', popupPhone.value);
+      localStorage.setItem('popupMessage', popupMessage.value);
     }
   }
 });
+
+
